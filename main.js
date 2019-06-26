28e26db0b74b801619c600982484d0aec3960c61
Pedro <pedrolps.ynwa@gmail.com>
2019-06-26 00:42:42 -0300

    add promises in the request

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
  
-const httpRequest = new XMLHttpRequest() // Instancia para recuperar dados de um servidor
-  const url = 'https://api.github.com/users/pedrosvg';
-  let user = null;
+const myPromise = _ => {
+  return new Promise( (resolve, reject) => { 
 
+    const httpRequest = new XMLHttpRequest() // Instancia para recuperar dados de um servidor
+    const url = 'https://api.github.com/users/pedrosvg';
+    let user = null;
 
-
-  function request(){
     httpRequest.open('GET', url)  // Estabelecer a
     httpRequest.send(null)
+
     
     httpRequest.onreadystatechange = _ => {
-    if(httpRequest.readyState === 4 ){
-       user = JSON.parse(httpRequest.responseText)
-        console.log(user.name)
+      if(httpRequest.readyState === 4 ){
+        if(httpRequest.status === 200){
+          resolve(JSON.parse(httpRequest.responseText))
+        }
+
+        else{
+          reject('Something bad happened :(')
+        }       
+      }
     }
-  }
+  })
 }
+
+  myPromise()
+    .then( response => console.log(response))
+    .catch( error => console.warn(error))
 
 
   const $button = document.getElementById('btn')
-
   $button.addEventListener('click', request, false)
+
 
 
 
